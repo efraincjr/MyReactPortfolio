8f1938d46f7eb88998d124d4b2d814f67991b681
Efrain Castro Jr <efraincastrojrGmail.com>
2023-02-05 15:11:58 -0600

    contact

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import spacesurfer from './space-surfer.png'
+
+export default function Contact() {
+  return (
+    <div class="light-text center fade">
+      <div class="vertical-fade"></div> 
+
+      <br></br>
+      <h1>Contact</h1>
+      <br></br>
+      <h5 className='intro-words-2'>For resume or other inquiries, please send requests to:</h5>
+      <br></br>
+      <p>
+        <h3>efraincastrojr@gmail.com</h3>
+      </p>
+      <br></br>
+      <img src={spacesurfer} alt="spaceman surfing" />
+      <div class="vertical-fade-reverse"></div>
+    </div>
+  );
+}
