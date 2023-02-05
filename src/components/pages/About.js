00c4a7c764365c02205bdf1f87daff78c7c0c182
Efrain Castro Jr <efraincastrojrGmail.com>
2023-02-05 15:01:02 -0600

    pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div class="light-text center fade">
+      <div class="vertical-fade"></div>
+
+      <div className='intro-words-2'>
+      <br></br>
+      <h1>About</h1>
+      <br></br>
+
+      <p> Strong problem solver with a team-oriented mindset: efficiently contributes consistent, functional, intelligent, clean, and descriptive code to my organization. Combining real world tech-industry experience with recently earned certificates from SMU Coding Bootcamp. Strives to consistently become a better software engineer by learning and applying new technologies to improve myself, skillset, projects, and to create as much value as possible for any company. </p>
+
+      <br></br>
+
+      <p>
+        Front-End · Back-End · QA · Test
+        <br></br>
+        <br></br>
+        MVC  ·  MERN
+        <br></br>
+        <br></br>
+        <div className='intro-words-2'>
+          JavaScript · React · HTML5 · Express · Bootstrap · HandlebarsJS · jQuery · Sequelize · GraphQL · MongoDB · SQL · Node
+        </div>
+        <br></br>
+      </p>
+      
+      </div>
+      <div class="vertical-fade-reverse"></div>
+    </div>
+  );
+}
